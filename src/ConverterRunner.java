8c950863cf18a2e19e0f7fe4edee625cd5fe565c
Wilson <wlei6251@bths.edu>
2023-02-02 22:37:40 -0500

    Added print statements for conversions

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -5,11 +5,25 @@ class ConverterRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
+        System.out.print("Enter the base of your number (2, 8, 10): ");
 
         Scanner s = new Scanner(System.in);
         String choice = s.nextLine();
+        while (!choice.equals("2"))
+        {
+            if (choice.equals("8"))
+            {
+                break;
+            }
+            if (choice.equals("10"))
+            {
+                break;
+            }
+            System.out.println("Try again");
+            choice = s.nextLine();
+        }
         int base = Integer.parseInt(choice);
+
 
         System.out.print("Enter your number: ");
         String number = s.nextLine();
@@ -22,7 +36,22 @@ class ConverterRunner {
         int[] digits = nc.getDigits();
         System.out.println("\n\nDigit array: " + Arrays.toString(digits));
         System.out.print("Number: " + nc.displayOriginalNumber());
-        System.out.println("Octal: " + Arrays.toString(nc.convertToOctal()));
+        if (base == 2)
+        {
+            System.out.println("Decimal: " + Arrays.toString(nc.convertToDecimal()));
+            System.out.println("Octal: " + Arrays.toString(nc.convertToOctal()));
+        }
+        if (base == 8)
+        {
+            System.out.println("Binary: " + Arrays.toString(nc.convertToBinary()));
+            System.out.println("Decimal: " + Arrays.toString(nc.convertToDecimal()));
+        }
+        if (base == 10)
+        {
+            System.out.println("Binary: " + Arrays.toString(nc.convertToBinary()));
+            System.out.println("Octal: " + Arrays.toString(nc.convertToOctal()));
+        }
+
     }
 }
 
